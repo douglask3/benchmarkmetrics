ed2e83be71fd5dd6eeb75a436240fc18d0973718
Doug Kelley <douglas.kelley@students.mq.edu.au>
2014-05-27 14:22:52 +1000

    quick correction in variance calculation, then allowng mean and varianace to be calculated per item for itemized metrics

--- setMetClassInfo.r
+++ setMetClassInfo.r
@@ -1,15 +1,37 @@
-setMetClassInfo <- function(out,x,y,w,varFun=absVar) {
+setMetClassInfo <- function(out,x,y,w,varFun=absVar,itemize=FALSE) {
 	out$x=x
 	out$y=y
 	out$w=w
 	
-	out$xMean=mean(x)
-	out$yMean=mean(y)
+	c(x,y):=setColNames(x,y)
+	
+	if (itemize) {
+		meanMe 	<- function(x,...)  apply(x,2,mean)
+		varMe 	<- function(x,...)  apply(x,2,absVar)
+	} else {
+		meanMe 	<- mean
+		varMe	<- varFun
+	}
+	
+	out$xMean=meanMe(x)
+	out$yMean=meanMe(y)
 	out$MeanRatio=out$yMean/out$xMean
 	
-	out$xVar=varFun(x)
-	out$yVar=varFun(y)
-	out$MeanRatio=out$xVar/out$yVar
-	
+	out$xVar=varMe(x)
+	out$yVar=varMe(y)
+	out$VarRatio=out$yVar/out$xVar
 	return(out)
+}
+
+setColNames <- function (x,y) {
+	if (is.null(colnames(x)) && is.null(colnames(y))) colnames(x)=letters[1:dim(x)[2]]
+	setColname <- function(x,y)	{
+		if (is.null(colnames(x))) colnames(x)=colnames(y)
+		return(x)
+	}
+	
+	x=setColname(x,y)
+	y=setColname(y,x)
+	
+	return(list(x,y))
 }
