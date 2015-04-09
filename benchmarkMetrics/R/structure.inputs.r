2bfbd0b24ce03b8b7617a9d488408f30b801cbbb
doug <douglas.kelley@students.mq.edu.au>
2015-04-09 15:46:04 +0100

    tidying:

--- benchmarkMetrics/R/structure.inputs.r
+++ benchmarkMetrics/R/structure.inputs.r
@@ -1,6 +1,7 @@
 structure.inputs <- function(x,y,w,itemize=FALSE) {	
 	x <- as.matrix(x)
 	y <- as.matrix(y)
+    
 	if (is.null(w))  if (itemize) w=array(1,dim(x)[1]) else w=array(1,dim(x))
 	
 	w=as.matrix(w)
