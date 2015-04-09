4370eac7117707d5146674109dc8fda85968d2e0
doug <douglas.kelley@students.mq.edu.au>
2015-04-09 19:14:02 +0100

    tidying

--- benchmarkMetrics/R/NMEForm.r
+++ benchmarkMetrics/R/NMEForm.r
@@ -1,2 +1,2 @@
-NMEForm <- function(x,y,w) sum(w*abs(y-x))/sum(w*abs(x-mean(x)))
+NMEForm  <- function(x,y,w) sum(w*abs(y-x))/sum(w*abs(x-mean(x)))
 NMSEForm <- function(x,y,w) sum(w*(y-x)^2)/sum(w*(x-mean(x))^2)
