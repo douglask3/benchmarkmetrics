2fabbd1f36469dcb5d55429c64b14e24601dd552
doug <douglas.kelley@students.mq.edu.au>
2015-04-10 15:09:41 +0100

    can cut out an arguemnt

--- benchmarkMetrics/R/structure.inputs.r
+++ benchmarkMetrics/R/structure.inputs.r
@@ -1,11 +1,12 @@
-structure.inputs <- function(x,y,w,itemize=FALSE,asVector=FALSE) {	
+structure.inputs <- function(x,y,w,itemize=FALSE) {	
     x <- as.matrix(x)
 	y <- as.matrix(y)
     
 	if (is.null(w))  if (itemize) w=array(1,dim(x)[1]) else w=array(1,dim(x))
 	w=as.matrix(w)
     
-    if (asVector) c(x,y,w):=lapply(list(x,y,w),function(i) matrix(i,ncol=1))
-	
+    if (!itemize) c(x,y,w):=lapply(list(x,y,w),function(i) matrix(i,ncol=1))
+    
 	return(list(x,y,w))
 }
+
