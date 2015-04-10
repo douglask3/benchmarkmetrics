96a2a93d9b1d37dc3c4da758fe19e403eb44847e
doug <douglas.kelley@students.mq.edu.au>
2015-04-10 13:53:47 +0100

    structing for raster to allow plots

--- benchmarkMetrics/R/structure.inputs.performNME.r
+++ benchmarkMetrics/R/structure.inputs.performNME.r
@@ -1,5 +1,5 @@
 structure.inputs.performNME <- function(x,y,w,...) {
-	c(x,y,w):=structure.inputs(x,y,w)
+	c(x,y,w):=structure.inputs(x,y,w,asVector=TRUE)
 	out=setNMEclassVars(x,y,w,...)	
 	return(out)
 }
--- benchmarkMetrics/R/structure.inputs.r
+++ benchmarkMetrics/R/structure.inputs.r
@@ -1,10 +1,11 @@
-structure.inputs <- function(x,y,w,itemize=FALSE) {	
-	x <- as.matrix(x)
+structure.inputs <- function(x,y,w,itemize=FALSE,asVector=FALSE) {	
+    x <- as.matrix(x)
 	y <- as.matrix(y)
     
 	if (is.null(w))  if (itemize) w=array(1,dim(x)[1]) else w=array(1,dim(x))
-	
 	w=as.matrix(w)
+    
+    if (asVector) c(x,y,w):=lapply(list(x,y,w),function(i) matrix(i,ncol=1))
 	
 	return(list(x,y,w))
 }
