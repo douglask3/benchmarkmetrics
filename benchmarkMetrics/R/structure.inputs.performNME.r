92d15758082464b6adf99f3943062a48800834c0
doug <douglas.kelley@students.mq.edu.au>
2015-04-10 16:08:29 +0100

    removing obsolute (and therefore crash causing) arguement

--- benchmarkMetrics/R/structure.inputs.performNME.r
+++ benchmarkMetrics/R/structure.inputs.performNME.r
@@ -1,5 +1,5 @@
 structure.inputs.performNME <- function(x,y,w,...) {
-	c(x,y,w):=structure.inputs(x,y,w,asVector=TRUE)
+	c(x,y,w):=structure.inputs(x,y,w)
 	out=setNMEclassVars(x,y,w,...)	
 	return(out)
 }
