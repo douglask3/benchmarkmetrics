4706b50e0f1b2ce89668ed65209f12d8a8881c7c
doug <douglas.kelley@students.mq.edu.au>
2015-04-12 21:58:09 +0100

    getting the angle right

--- benchmarkMetrics/R/PolarConcentrationAndPhase.r
+++ benchmarkMetrics/R/PolarConcentrationAndPhase.r
@@ -1,16 +1,18 @@
 PolarConcentrationAndPhase <- function(cdata,phase_units="radians",n=dim(cdata)[2]) {
-    a=dim(cdata)[2]
+    if (class(cdata)=="numeric") cdata =  t(matrix(cdata))
+    a = dim(cdata)[2]
     
-    xdata=ydata=matrix(0,dim(cdata)[1],1)
+    xdata = ydata = matrix(0, dim(cdata)[1], 1)
     for (k in 1:a) {
-        angle=2*pi*(n-k+1)/n
-        xdata=xdata+cdata[,k]*cos(angle)
-        ydata=ydata+cdata[,k]*sin(angle)
+        angle = 2 * pi * (n - k + 1) / n
+        xdata = xdata + cdata[, k] * cos(angle)
+        ydata = ydata + cdata[, k] * sin(angle)
     }
     
-    adata=sum(cdata)
+    adata = apply(cdata,1,sum)
     
-    pdata=atans(0-ydata,0-xdata,phase_units)
-    cdata=sqrt(xdata^2+ydata^2)/adata
-    list(pdata,cdata)
+    phase = atans(0 - ydata, 0 - xdata, phase_units)
+    phase = atans(-ydata, xdata, phase_units)
+    conc  = sqrt (xdata^2 + ydata^2) / adata
+    return(list(phase, conc))
 }
--- benchmarkMetrics/R/atans.r
+++ benchmarkMetrics/R/atans.r
@@ -1,17 +1,17 @@
-atans <- function(x,y,units='months') {
+atans <- function(x,y,units='months',revs=FALSE) {
     if (class(x)=="RasterLayer") phase_out=x[[1]]
     
     if (class(x)=="RasterLayer") x=as.matrix(x)
     if (class(y)=="RasterLayer") y=as.matrix(y)
     
     phase=atan2(x,y)
-
+    
     if (units=='months') {
       phase=6*(phase/pi)+6;
     } else if (units=='degrees') { 
-      phase=phase+pi
+      phase=phase
       phase=phase*360/(2*pi)
-    } else if (units=='radians') phase=phase+pi
+    } else if (units=='radians') phase=phase
             
     if (class(x)=="RasterLayer") {
     	values(phase_out)=phase
