dda545a877f35c7d93563dccccf89f56ac97d87e
doug <douglas.kelley@students.mq.edu.au>
2015-04-12 17:52:19 +0100

    period length based on matrixshape not hard-coded to 12

--- benchmarkMetrics/R/seasonal_concentration_and_phase.r
+++ benchmarkMetrics/R/seasonal_concentration_and_phase.r
@@ -1,13 +1,13 @@
-seasonal_concentration_and_phase <- function(cdata,phase_units="radians") {
-
+seasonal_concentration_and_phase <- function(cdata,phase_units="radians",n=dim(cdata)[2]) {
     a=dim(cdata)[2]
     
     xdata=ydata=matrix(0,dim(cdata)[1],1)
     for (k in 1:a) {
-        angle=2*pi*(12-k+1)/12
+        angle=2*pi*(n-k+1)/n
         xdata=xdata+cdata[,k]*cos(angle)
         ydata=ydata+cdata[,k]*sin(angle)
     }
+    
     adata=sum(cdata)
     
     pdata=atans(0-ydata,0-xdata,phase_units)
