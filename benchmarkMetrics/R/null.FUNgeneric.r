7652989025237d08ae572b9986e4c0fa86bbd8a8
doug <douglas.kelley@students.mq.edu.au>
2015-04-15 16:07:29 +0100

    tidying and commenting

--- benchmarkMetrics/R/null.FUNgeneric.r
+++ benchmarkMetrics/R/null.FUNgeneric.r
@@ -27,31 +27,40 @@ print.NullModelSummary <- function(x) {
 }
 
 plot.nullModel <- function(x,xlab='',ylab='',...) {
+    # Plot histergram of random model
     hist(x[[2]],ceiling(length(x[[2]])/10),yaxt='n',xlab=xlab,ylab=ylab)
     
+    # Calculate summary of null scores
     x=summary(x)
     randRange =  x[2]+x[3]*c(-1,1)
     
+    # Plot std range as oligon
     polygon(rep(randRange,each=2),c(0,1,1,-1)*9E9,
             border=NA,col=makeTransparent('blue',0.8))
     
+    # Plot Mean mode in read; mean random-resampled socre in blue, and 
+    #sd range of randon resampled score in dashed blue
     verLine <- function(xi,col='blue',...) lines(rep(xi,2),c(0,9E9),col=col,...)
     
     verLine(x[1],'red')
     verLine(x[2])
     verLine(x[2]-x[3],lty=2)
     verLine(x[2]+x[3],lty=2)
-    
-    
-    legendStandard <- function(...)  legend('topleft',bty='n',legtxt,...)
+}
+
+nullModelLegend <- function() {
+    ## Define legend arrangeent and labels
+     legendStandard <- function(...)  legend('topleft',bty='n',legtxt,...)
     
     legtxt=c('Mean Model','Randon Model',paste('    ',c('frequancy','mean','standard deviation')))
     
+    ## Add lines and symbols
     lty=c(1,0,0,1,2)
     pch=c(NA,0,0,NA,NA)
     col=c('red','transparent','black','blue','blue')
     legendStandard(lty=lty,pch=pch,col=col)
     
+    ## Add polygn shades
     pch=15
     col = c('transparent','transparent','transparent','blue','blue')
     col = makeTransparent(col,0.8)
@@ -59,5 +68,3 @@ plot.nullModel <- function(x,xlab='',ylab='',...) {
     lty[length(lty)]=0
    
     legendStandard(lty=lty,pch=15,col=col,y.intersp=c(rep(1,4),0.97))
-    
-}
