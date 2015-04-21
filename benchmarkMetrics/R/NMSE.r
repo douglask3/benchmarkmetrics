7c6d65102f2fdc4a33b4933d9b004a142cb688dd
doug <douglas.kelley@students.mq.edu.au>
2015-04-21 17:49:15 +0100

    passing arguments

--- benchmarkMetrics/R/NMSE.r
+++ benchmarkMetrics/R/NMSE.r
@@ -1,7 +1,7 @@
 NMSE <- function(x,...) UseMethod("NMSE")
 
-NMSE.default <- function(x,y,w=NULL) {
-	out=structure.inputs.performNME(x,y,w,metFun=NMSEForm,varFun=sd)
+NMSE.default <- function(x,y,w=NULL,...) {
+	out=structure.inputs.performNME(x,y,w,metFun=NMSEForm,varFun=sd,...)
 	out$call <- match.call()
 	return(out)
 }
