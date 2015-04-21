af75e05f1b658a8401eb586db4242895616b144b
doug <douglas.kelley@students.mq.edu.au>
2015-04-21 18:02:35 +0100

    no passing arguemnts through mean test as arguments affect if step 1 2 or 3 is taken, and for meean all 3 required

--- benchmarkMetrics/R/null.FUN.r
+++ benchmarkMetrics/R/null.FUN.r
@@ -3,7 +3,7 @@ null.FUN <- function(x,...) UseMethod("null.FUN")
 null.FUN.default <- function(x, FUN, n = 1000, items = FALSE, ...) {
 	x <- structure.inputs.nulls(x, items)
     
-    mscore = meanMod.FUN(x, FUN,    ...)
+    mscore = meanMod.FUN(x, FUN)
     rscore = randMod.FUN(x, FUN, n, ...)
     
     out = list(meanModel = mscore, randModel = rscore)
