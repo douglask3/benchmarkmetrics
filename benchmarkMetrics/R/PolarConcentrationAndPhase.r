a247da73bdac182cc13b29c0b9e343735583582d
doug <douglas.kelley@students.mq.edu.au>
2015-04-21 17:39:39 +0100

    fixing typo from old lines

--- benchmarkMetrics/R/PolarConcentrationAndPhase.r
+++ benchmarkMetrics/R/PolarConcentrationAndPhase.r
@@ -1,9 +1,8 @@
 PolarConcentrationAndPhase <- function(cdata,phase_units="radians",n=dim(cdata)[2]) {
     if (class(cdata)=="numeric") cdata =  t(matrix(cdata))
-    a = dim(cdata)[2]
     
     xdata = ydata = matrix(0, dim(cdata)[1], 1)
-    for (k in 1:a) {
+    for (k in 1:n) {
         angle = 2 * pi * (n - k + 1) / n
         xdata = xdata + cdata[, k] * cos(angle)
         ydata = ydata + cdata[, k] * sin(angle)
