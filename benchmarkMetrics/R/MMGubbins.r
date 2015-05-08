ca7971061485ff8ea8205cdbfbb74977c182619b
Douglas Kelley <douglas.kelley@mq.edu.au>
2015-05-09 07:01:52 +1000

    tidy

--- benchmarkMetrics/R/MMGubbins.r
+++ benchmarkMetrics/R/MMGubbins.r
@@ -2,7 +2,8 @@ check.and.norm.performMM <- function(x,y,w,...) {
 	c(x,y,w):=structure.inputs(x,y,w)
 	normalise <- function(x) sweep(x,1,rowSums(x),'/')
 	
-	if (dim(x)[2]<2 && dim(y)[2]<2) stop("2 or more items required")
+	if (dim(x)[2]<2 && dim(y)[2]<2)
+	    stop("2 or more items required")
 	
 	w0=w
 	if (dim(w)[2]==1) for (i in 1:(dim(x)[2]-1)) w=r=cbind(w,w0)
