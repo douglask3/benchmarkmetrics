39d07c893fd738ed55ef4f76f225e155508c5a5a
Douglas Kelley - Reading Linux <douglas.i.kelley@gmail.com>
2015-07-22 11:37:38 +0100

    slight bug when building package whereby raster is not converted to matrix without first accessing values. Not sure what caused the buig - works fine when libs are sourced regularly - but an easy fix is to find values first if varibale is of class raster (hence the inclusion 9of an is.raster lib, taken from douglask3/lpx_futures2)

--- benchmarkMetrics/R/structure.inputs.r
+++ benchmarkMetrics/R/structure.inputs.r
@@ -1,18 +1,19 @@
-structure.inputs <- function(x, y, w, itemize = FALSE, na.rm = TRUE) {	    
+structure.inputs <- function(x, y, w, itemize = FALSE, na.rm = TRUE) {
+
     x <- setAsMatrix(x)
 	y <- setAsMatrix(y)
     
     if (!all(dim(x) == dim(y))) y = matchDimensions(x, y)
-    
+
 	if (is.null(w))
         if (itemize) w = array(1, dim(x)[1]) else w = array(1,dim(x))
 	w = as.matrix(w)
-    
+
     if (!itemize) c(x, y, w) := lapply(list(x, y, w),function(i)
                                                 matrix(i, ncol = 1))
 	if (na.rm) {
         test = !is.na(apply(x,1,sum)+apply(y,1,sum))
-        
+
         x = as.matrix(x[test, ])
         y = as.matrix(y[test, ])
         w = as.matrix(w[test, ])
@@ -20,19 +21,23 @@ structure.inputs <- function(x, y, w, itemize = FALSE, na.rm = TRUE) {
     return(list(x, y, w))
 }
 
-setAsMatrix <- function(x) 
-    if (class(x) == "ts") return(ts2matrix(x)) else return(as.matrix(x))
+setAsMatrix <- function(x) {
+    if (class(x) == "ts") return(ts2matrix(x)) else {
+        if (is.raster(x)) x = values(x)
+        return(as.matrix(x))
+    }
+}
 
 matchDimensions <- function(x, y, y0 = y, tryC = 0) {
     if (tryC == max(sapply(list(x, y), function(i) length(dim(i)))))
         return(y0)
-     
+
     tryC = tryC + 1
     if (!any(dim(x) == dim(y))) {
         y = aperm(y)
         matchDimensions(x, y, y0, tryC)
     }
-    
+
     y = t(matrix(rep(y, length.out = length(x)), ncol = nrow(x)))
-    return(y)      
+    return(y)
 }
