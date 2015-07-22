113038c410e8442425208d9e19b7f990f9003539
Douglas Kelley - Reading Linux <douglas.i.kelley@gmail.com>
2015-07-22 11:38:35 +0100

    adding missing function

---
+++ benchmarkMetrics/R/ts2matrix.r
@@ -0,0 +1,12 @@
+ts2matrix <- function(x) {
+    f = frequency(x)
+    t = tsp(x)
+    y = t(matrix(x, nrow = f))
+
+    if (f == 12) colnames(y) = month.abb
+    if (f ==  4) colnames(y) = c('Qtr1', 'Qtr2', 'Qtr3', 'Qtr4')
+
+    rownames(y) = seq(t[1], t[2] + 1/f - 1, length.out = nrow(y))
+
+    return(y)
+}
