29495435c7888e563775c1bb7ad197211ad335b4
Douglas Kelley - Reading Linux <douglas.i.kelley@gmail.com>
2015-07-29 22:54:05 +0100

    tidyinhg

--- benchmarkMetrics/R/print.MetricSummary.r
+++ benchmarkMetrics/R/print.MetricSummary.r
@@ -1,16 +1,30 @@
 print.MetricSummaryList  <- function(x, ...) {
-	printMetricHead(x[[1]]); printMetricHead(x[2], '\t')
+	printMetricCat(x[[1]]); printMetricCat(x[2], '\t')
 	lapply(x[-1:-2], print.MetricSummary)
 	invisible(x)
 }
 
 print.MetricSummary <- function(x, ...) {
+	printMetricSummaryHead(x)
+	printMeanVarSummaryTable(x)
+	cat("\n")
+	printMetricSummaryScores(x)
+	cat("\n")
+	invisible(x)
+}
+
+printMetricCat <- function(x, strt = "")
+	cat(strt, names(x),': ', x[[1]], '\n', sep =" ")
+
+printMetricSummaryHead <- function(x) {
 	cat("\n==============================\n")
-	printMetricHead(x[1])
+	printMetricCat(x[1])
 	cat(  "==============================\n")
 	weightID = names(x) == 'weights'
-	if (any(weightID)) printMetricHead(x[weightID])
+	if (any(weightID)) printMetricCat(x[weightID])
+}
 
+printMeanVarSummaryTable <- function(x) {
 	cat(' Basic Variable Information:\n')
 	MeanVars = unlist(x[c('xMean',     'yMean',     'x:y Mean ratios',
 	                      'xVariance', 'yVariance', 'x:y Variance ratios')])
@@ -19,13 +33,9 @@ print.MetricSummary <- function(x, ...) {
 	rownames(MeanVars) = c(' Mean',' Variance')
 
 	print(MeanVars)
-
-	cat("\n")
-	x[['Scores']] = standard.round(x[['Scores']])
-	printMetricHead(x['Scores'])
-	cat("\n")
-	invisible(x)
 }
 
-printMetricHead <- function(x, strt = "")
-	cat(strt, names(x),': ', x[[1]], '\n', sep =" ")
+printMetricSummaryScores <- function(x) {
+	x[['Scores']] = standard.round(x[['Scores']])
+	printMetricHead(x['Scores'])
+}
