ee7cd8c5c554ae335edacc59427cb375ce49ecb3
Douglas Kelley - Reading Linux <douglas.i.kelley@gmail.com>
2015-08-03 16:18:51 +0100

    tidying

--- benchmarkMetrics/R/NME.r
+++ benchmarkMetrics/R/NME.r
@@ -1,8 +1,7 @@
 NME <- function(x, ...) UseMethod("NME")
 
-NME.default <- function(x, y, w=NULL, ...) {
+NME.default <- function(x, y, w = NULL, ...) {
 	out = structure.inputs.performNME(x, y, w, ...)
 	out$call <- match.call()
 	return(out)
 }
-
