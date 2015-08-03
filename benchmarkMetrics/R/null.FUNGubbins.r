8956c4a005a3c935aec8f055a20111ac66f78b65
Douglas Kelley - Reading Linux <douglas.i.kelley@gmail.com>
2015-08-03 14:09:30 +0100

    masking out NaNs in null model comparions

--- benchmarkMetrics/R/null.FUNGubbins.r
+++ benchmarkMetrics/R/null.FUNGubbins.r
@@ -1,5 +1,6 @@
 meanMod.FUN <- function(x, FUN, ...) {
     meanX = apply(x, 2, mean)
+
     return(score(FUN(x, meanX, ...)))
 }
 
@@ -14,5 +15,6 @@ randMod.FUN <- function(x, FUN, nrs=1000, ...) {
 structure.inputs.nulls <- function(x, items = FALSE) {
     x = setAsMatrix(x)
     if (!items) x = as.matrix(as.vector(x))
+    x = as.matrix(x[!is.na(apply(x,1,sum)),])
     return(x)
 }
