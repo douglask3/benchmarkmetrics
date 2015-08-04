6cd09e93cbcd998b11385d15cf8cdf9e761826cf
Douglas Kelley - Reading Linux <douglas.i.kelley@gmail.com>
2015-08-04 18:07:29 +0100

    sturtureing weights in null model input struicture as well tyo make sure weights work

--- benchmarkMetrics/R/null.FUN.r
+++ benchmarkMetrics/R/null.FUN.r
@@ -3,7 +3,7 @@ null.FUN <- function(x, ...) UseMethod("null.FUN")
 null.FUN.default <- function(x, FUN, w = NULL, n = 1000, items = FALSE,
 	                         step1only = c(TRUE, TRUE),  ...) {
 
-	x <- structure.inputs.nulls(x, items)
+	c(x, w) := structure.inputs.nulls(x, w, items)
 
     mscore = meanMod.FUN(x, w, FUN,    step1only = step1only[1], ...)
     rscore = randMod.FUN(x, w, FUN, n, step1only = step1only[2], ...)
--- benchmarkMetrics/R/null.FUNGubbins.r
+++ benchmarkMetrics/R/null.FUNGubbins.r
@@ -6,15 +6,31 @@ meanMod.FUN <- function(x, w = NULL, FUN, ...) {
 
 randMod.FUN <- function(x, w = NULL, FUN, nrs=1000, ...) {
     randComp <- function(i) {
+
         randX = x[sample(1:dim(x)[1]),]
         return(score(FUN (x, randX, w, ...)))
     }
     return(sapply(1:nrs, randComp))
 }
 
-structure.inputs.nulls <- function(x, items = FALSE) {
+structure.inputs.nulls <- function(x, w, items = FALSE, allowRegridding = TRUE) {
+    wNtNull = !is.null(w)
+    if (allowRegridding && wNtNull &&
+        is.raster(x) && is.raster(w))
+        c(x, w) := cropInputs(x, w)
     x = setAsMatrix(x)
-    if (!items) x = as.matrix(as.vector(x))
-    x = as.matrix(x[!is.na(apply(x,1,sum)),])
-    return(x)
+    
+    if (wNtNull) w = setAsMatrix(w)
+
+    if (!items) {
+        x = as.matrix(as.vector(x))
+        if (wNtNull) w = as.matrix(as.vector(w))
+    }
+
+    mask = !is.na(apply(x,1,sum))
+    if (wNtNull) mask = mask & !is.na(apply(w,1,sum))
+
+    x = as.matrix(x[mask,])
+    if (wNtNull) w = as.matrix(w[mask,])
+    return(list(x, w))
 }
