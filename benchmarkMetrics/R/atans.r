552f5f95a0d0d6a59aed865069b80f2e2b070638
douglask3 - Reading <douglas.i.kelley@gmail.com>
2015-10-16 11:24:41 +0100

    correcting monthly unit correction so goes 0 - 12 with 0 maxing if minth 0 is at max

--- benchmarkMetrics/R/atans.r
+++ benchmarkMetrics/R/atans.r
@@ -1,19 +1,21 @@
 atans <- function(x, y, units = 'months', revs = FALSE) {
     classX = class(x)
-    
+
     if (classX   == "RasterLayer") phase_out = x[[1]]
     if (classX   == "RasterLayer") x = as.matrix(x)
     if (class(y) == "RasterLayer") y = as.matrix(y)
-    
+
     phase = atan2(x, y)
-    
+
     if (units == 'months') {
-      phase = 6 * (phase / pi) + 6
-    } else if (units == 'degrees') { 
-      phase = phase
-      phase = phase * 360 / (2 * pi)
+        phase = 6 * (phase / pi)
+        test = !is.na(phase) & phase < 0 
+        phase[test] = phase[test] + 12
+    } else if (units == 'degrees') {
+        phase = phase
+        phase = phase * 360 / (2 * pi)
     } else if (units == 'radians') phase = phase
-            
+
     if (classX == "RasterLayer") {
     	values(phase_out) = phase
     	return(phase_out)
