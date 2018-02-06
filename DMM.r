dce98a3960b44714d4852760e4ec68980b5924ba
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-02-06 17:02:24 +0000

    updarting example to show use of item names

--- DMM.r
+++ DMM.r
@@ -1,15 +1,51 @@
 library(gitBasedProjects)
 sourceAllLibs('benchmarkMetrics/R/')
+graphics.off()
 
-mat = 'benchmarkMetrics/data/biomeAffinityMat.csv'
+	###################
+	## Set up inputs ##
+	###################
+	## biomeAffinityMatrix is an example dataset provided as part of the package
+	print(biomeAffinityMatrix)
 
-obs = c(1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4)
-sim = c(1, 2, 3, 3, 4, 2, 1, 2, 3, 2, 1, 1, 2, 3, 3, 5, 2, 4, 5)
+	## bit of a random example of items numbers
+	obs = c(1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4)
+	sim = c(1, 2, 3, 3, 4, 2, 1, 2, 3, 2, 1, 1, 2, 3, 3, 5, 2, 4, 5)
 
-null = null.DMM(mat, obs)
-plot(null, legend = TRUE)
-
-out = DMM(mat, obs, sim)
-print(summary(out))
-boxplot(out)
-plot(out)
+	####################
+	## run and output ##
+	####################
+	out = DMM(biomeAffinityMatrix, obs, sim)
+	
+	print(out)
+	cat("\n=======\n")
+	print(score(out))
+	cat("\n=======\n")
+	print(summary(out))
+	cat("\n=======\n\n")	
+	
+	## can feed in biome names instead
+	obs = biomeAffinityMatrix[obs,1]
+	sim = biomeAffinityMatrix[sim,1]	
+	print(rbind(obs = obs, sim = sim))
+	
+	## to get the same result
+	out = DMM(biomeAffinityMatrix, obs, sim)
+	cat("\n")
+	print(out)
+	
+	##################
+	## plot results ##
+	##################
+	par(mfrow = c(2,1), mar = c(2, 4, 0, 1))
+	boxplot(out)
+	plot(out)
+	
+	#########################
+	## Run the null models ##
+	#########################
+	null = null.DMM(biomeAffinityMatrix, obs, n = 100)
+	cat("\n\n Null Model: \n")
+	print(summary(null))
+	dev.new()
+	plot(null, legend = TRUE)
