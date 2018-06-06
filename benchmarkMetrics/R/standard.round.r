0e4d779461d0adc2045fae654024fbc1ed0d2867
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-06-06 14:48:34 +0100

    allowing no. digit standard round

--- benchmarkMetrics/R/standard.round.r
+++ benchmarkMetrics/R/standard.round.r
@@ -1,10 +1,11 @@
-standard.round <- function(x) sapply(x, standard.round.ind)
+standard.round <- function(x, ...) sapply(x, standard.round.ind, ...)
 
-standard.round.ind <- function(x) {
+standard.round.ind <- function(x, n = 2) {
 	if (is.na(x)) 	return(x)
 	ndig = find.ndig(x)
-	if (ndig == 0)  return(signif(x,2))
-	if (ndig == 1)  return(round(x,2))
+	
+	if (ndig == 0)  return(signif(x,n))
+	if (ndig == 1)  return(round(x,n))
 	
 					return(round(x))
 }
